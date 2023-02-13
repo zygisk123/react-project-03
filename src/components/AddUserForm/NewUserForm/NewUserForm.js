cc13d5895dbb7d2b4cfe9977cf54cda64b443560
zygisk123 <zygimantask13@gmail.com>
2023-02-13 11:58:31 +0200

    style changes

--- src/components/AddUserForm/NewUserForm/NewUserForm.js
+++ src/components/AddUserForm/NewUserForm/NewUserForm.js
@@ -1,27 +1,20 @@
 import style from "./NewUserForm.module.css";
 import Button from "../../UI/Button/Button";
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import Card from "../../UI/Card/Card";
 import ErrorModal from "../../UI/ErrorModal/ErrorModal";
 const NewUserForm = (props) =>
 {
-    const [enteredUsername, setEnteredUsername] = useState('');
-    const [enteredAge, setEnteredAge] = useState('');
+    const nameInputRef = useRef();
+    const ageInputRef = useRef();
+
     const [error, setError] = useState();
-
-    const titleChangeHandler = (event) =>
-    {
-        setEnteredUsername(event.target.value);
-    }
-
-    const ageChangeHandler = (event) =>
-    {
-        setEnteredAge(event.target.value);
-    }
 
     const userFormSubmitHandler = (event) =>
     {
         event.preventDefault();
+        const enteredUsername = nameInputRef.current.value;
+        const enteredAge = ageInputRef.current.value;
         if (enteredUsername.trim().length === 0 ||
             enteredAge.trim().length === 0)
         {
@@ -47,9 +40,10 @@ const NewUserForm = (props) =>
             age: enteredAge,
             //id: Math.random().toString()
         }
-        setEnteredUsername('');
-        setEnteredAge('');
+
         props.onFormSubmit(userData);
+        nameInputRef.current.value = '';
+        ageInputRef.current.value = '';
     }
 
     const errorHandler = () =>
@@ -63,9 +57,9 @@ const NewUserForm = (props) =>
             <Card className={style.input}>
                 <form onSubmit={userFormSubmitHandler}>
                     <label htmlFor='username'>Username</label>
-                    <input id='username' type = "text" value={enteredUsername} onChange={titleChangeHandler}/>
+                    <input id='username' type = "text" ref={nameInputRef}/>
                     <label htmlFor="age">Age</label>
-                    <input id='age' type = "number"  step="1" value={enteredAge} onChange={ageChangeHandler}/>
+                    <input id='age' type = "number"  step="1" ref={ageInputRef}/>
                     <Button type="submit">
                         Add User
                     </Button>
